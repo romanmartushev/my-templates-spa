397d26f34809daf140b4e86be7bd8d40cb89dd76
Roman <romanm@paulbunyan.net>
2020-02-25 14:34:51 -0600

    some changes to the services section

--- resources/js/components/about/service.vue
+++ resources/js/components/about/service.vue
@@ -1,38 +1,32 @@
 <template>
-    <div class="w-full my-5">
-        <div class="bg-gray-700 text-gray-100 py-5">
+    <div class="w-full">
+        <div class="text-gray-100 py-20 bg-blue-500 mb-20">
             <div class="text-center">
                 <h2 class="text-6xl">Need a website that is:</h2>
             </div>
             <div class="flex flex-col md:flex-row justify-between text-center px-2">
                 <div class="hover:shadow-2xl p-3">
                     <div class="mt-5 mx-auto">
-                        <i class="fas fa-dragon fa-4x mb-3 text-blue-400"></i>
-                        <h3 class="mb-3 text-2xl">Custom Made</h3>
-                        <p class="text-gray-500 mb-0">No matter the idea, I can make it happen</p>
+                        <i class="fas fa-dragon fa-4x mb-3 text-red-300"></i>
+                        <h3 class="mb-3 text-2xl underline">&nbsp;&nbsp;Custom Made&nbsp;&nbsp;</h3>
+                        <p class="text-gray-300 mb-0">No matter the idea, I can make it happen</p>
                     </div>
                 </div>
                 <div class="hover:shadow-2xl p-3">
                     <div class="mt-5 mx-auto">
-                        <i class="fas fa-mobile fa-4x mb-3 text-blue-400"></i>
-                        <h3 class="mb-3 text-2xl">Mobile Friendly</h3>
-                        <p class="text-gray-500 mb-0">It will look good no matter what device you use</p>
+                        <i class="fas fa-mobile fa-4x mb-3 text-red-300"></i>
+                        <h3 class="mb-3 text-2xl underline">&nbsp;&nbsp;Mobile Friendly&nbsp;&nbsp;</h3>
+                        <p class="text-gray-300 mb-0">It will look good no matter what device you use</p>
                     </div>
                 </div>
                 <div class="hover:shadow-2xl p-3">
                     <div class="mt-5 mx-auto">
-                        <i class="fas fa-code fa-4x mb-3 text-blue-400"></i>
-                        <h3 class="mb-3 text-2xl">Up to Date</h3>
-                        <p class="text-gray-500 mb-0">Keep your site up to date with the latest and greatest</p>
+                        <i class="fas fa-code fa-4x mb-3 text-red-300"></i>
+                        <h3 class="mb-3 text-2xl underline">&nbsp;&nbsp;Up to Date&nbsp;&nbsp;</h3>
+                        <p class="text-gray-300 mb-0">Keep your site up to date with the latest and greatest</p>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="text-3xl text-gray-700 w-full mt-5 py-4 px-12">
-            <p>
-                Do you need a website for your growing company? My web development and design process will allow for a
-                finished product that is sure to catch the eye. Just check out my <a href="#recent-projects"><u>recent projects</u></a>!
-            </p>
         </div>
     </div>
 </template>
