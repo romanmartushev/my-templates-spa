6138d32d70ec92d6fb28b10dc2d97e374c244e7a
Roman <romanm@paulbunyan.net>
2020-02-25 13:57:56 -0600

    updated profile

--- resources/js/components/about/profile.vue
+++ resources/js/components/about/profile.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="container text-gray-100 mx-auto pt-20">
-        <div class="flex flex-col px-2 justify-center">
-            <img class="mb-3 mb-lg-0 rounded lg:w-3/4 h-auto mx-auto shadow-2xl" src="/images/profile_long.jpg" alt="">
+    <div class="container text-gray-100 mx-auto h-screen">
+        <div class="flex flex-col px-2 justify-center h-full">
+            <img class="mb-3 mb-lg-0 rounded lg:w-3/4 h-auto mx-auto shadow-2xl" src="/images/profile_long.jpg" alt="Roman Martushev">
             <div class="bg-gray-700 opacity-75 lg:hover:opacity-100 p-16 rounded lg:absolute shadow-2xl">
                 <h2 class="mb-4">
                     <span class="text-2xl">Roman Martushev</span>
@@ -25,5 +25,5 @@
 		components: {
 			Social
 		}
-	}
+    }
 </script>
