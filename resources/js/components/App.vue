1cd9c1609ae8dae78be151884541dbf77dd7cfae
Roman <romanm@paulbunyan.net>
2020-09-18 11:17:55 -0500

    updated footer link highlights

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -25,15 +25,14 @@
             <div class="container mx-auto">
                 <div class="bg-blue-500 rounded pl-2" id="recent-projects">Recent Projects</div>
                 <ul class="list-inside">
-                    <li class="border-b border-gray-800 py-2 px-6"><a href="https://livewire-thermostat.herokuapp.com/" target="_blank">Thermostat</a></li>
-                    <li class="border-b border-gray-800 py-2 px-6"><a href="http://www.tforceconstruction.com/" target="_blank">TriForce</a></li>
-                    <li class="border-b border-gray-800 py-2 px-6"><a href="http://myfamily.pbndev.net/" target="_blank">Ivanov Family Tree</a></li>
-                    <li class="border-b border-gray-800 py-2 px-6"><a href="http://family.romanmartushev.com/" target="_blank">Martushev Family Tree</a></li>
-                    <li class="border-b border-gray-800 py-2 px-6"><a href="https://stonegate-exteriors.herokuapp.com/" target="_blank">STONEGATE EXTERIORS (in development)</a></li>
-                    <li class="border-b border-gray-800 py-2 px-6"><a href="https://ibc-company-llc.herokuapp.com/" target="_blank">IBC Company LLC. (in development)</a></li>
-                    <li class="border-b border-gray-800 py-2 px-6"><a href="https://shortcode-form-builder.herokuapp.com/views/form-builder.html" target="_blank">Form [short-code] Builder (for development)</a></li>
-                    <li class="border-b border-gray-800 py-2 px-6"><a href="https://gigazonegaming.com/" target="_blank">Gigazone Gaming</a></li>
-                    <li class="border-b border-gray-800 py-2 px-6"><a href="https://paulbunyan.net/" target="_blank">Paul Bunyan Communications</a></li>
+                    <li class="footer-item"><a href="https://mary-maids-cleaning-services.romanmartushev.com/" target="_blank">Mary Maids Cleaning Services</a></li>
+                    <li class="footer-item"><a href="http://www.tforceconstruction.com/" target="_blank">TriForce</a></li>
+                    <li class="footer-item"><a href="http://myfamily.pbndev.net/" target="_blank">Ivanov Family Tree</a></li>
+                    <li class="footer-item"><a href="http://family.romanmartushev.com/" target="_blank">Martushev Family Tree</a></li>
+                    <li class="footer-item"><a href="https://stonegate-exteriors.herokuapp.com/" target="_blank">STONEGATE EXTERIORS (in development)</a></li>
+                    <li class="footer-item"><a href="https://ibc-company-llc.herokuapp.com/" target="_blank">IBC Company LLC. (in development)</a></li>
+                    <li class="footer-item"><a href="https://gigazonegaming.com/" target="_blank">Gigazone Gaming</a></li>
+                    <li class="footer-item"><a href="https://paulbunyan.net/" target="_blank">Paul Bunyan Communications</a></li>
                 </ul>
             </div>
             <div class="pt-2">
